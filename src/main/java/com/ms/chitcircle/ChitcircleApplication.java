caab7e16e13a6cb9a44d19c60692908a109d1653
sreekeerthi-targix <sreekeerthi.m@targix.in>
2026-08-23 12:32:42 +0530

    first commit

---
+++ src/main/java/com/ms/chitcircle/ChitcircleApplication.java
@@ -0,0 +1,13 @@
+package com.ms.chitcircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChitcircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChitcircleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/chitcircle/ChitcircleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.chitcircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChitcircleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
